f01d030aa31440358be1ade49956f5f11d05b11c
MD FOYSAL <foysal8353@gmail.com>
2024-12-26 18:33:59 +0600

    commit all

--- practice_selenium/firstautomation.py
+++ practice_selenium/firstautomation.py
@@ -5,7 +5,7 @@ chrome_driver_path = r"chromedriver.exe"
 chrome_options=Options()
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service = service, options = chrome_options)
-driver.get("https://www.rcvacademy.com")
+driver.get("https://www.bdjobs.com/")
 driver.maximize_window()
 print(driver.title)
 driver.close()
